3c5b5b58803a0f2f9c0545cecc7bbd5eca75110f
Kelsi Olstad <kelsiolstad@gmail.com>
2022-04-05 15:10:09 -0600

    add current player and labels

--- classes/question.rb
+++ classes/question.rb
@@ -1,26 +1,23 @@
 class Question
 attr_reader :number_1, :number_2, :response
 
-  def initialize
+  def initialize current_player
   @number_1 = rand(21)
   @number_2 = rand(21)
+  @current_player = current_player
   end
 
-  def question
-   puts "What does #{number_1} plus #{number_2} equal?"
-   $response = gets.chomp.to_i
-   puts "The response is: #{$response}"
+  def question 
+   puts "#{@current_player.name}: What does #{number_1} plus #{number_2} equal?"
+   @response = gets.chomp.to_i
   end
 
   def check_response
-    if (@number_1 + @number_2) == $response
-      puts "YES! You are correct."
+    if (@number_1 + @number_2) == @response
+      puts "#{@current_player.name}: YES! You are correct."
     else
-      puts "NO! You are incorrect."
+      puts "#{@current_player.name}: NO! You are incorrect."
+      @current_player.take_life 
     end
   end
 end
-
-question1 = Question.new
-puts question1.question
-puts question1.check_response
