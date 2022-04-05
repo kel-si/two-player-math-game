53edb09ed7357c305af795b182bbc661773d8994
Kelsi Olstad <kelsiolstad@gmail.com>
2022-04-05 15:09:14 -0600

    create question call, prompt for user, update score, set current player, and decide winner

--- classes/game.rb
+++ classes/game.rb
@@ -4,12 +4,39 @@ class Game
 
   def initialize name
     @name = name
-    @p1 = Player.new 'P1'
-    @p2 = Player.new 'P2'
+    @p1 = Player.new 'Player 1'
+    @p2 = Player.new 'Player 2'
+    @current_player = @p1
   end
 
-  def check_answer
+  def play
+    while @p1.score != 0 && @p2.score != 0 
+      # create question
+      q = Question.new @current_player
 
+      # prompt user
+      puts "---- NEW TURN ----"
+      q.question
+      q.check_response
+
+      # update score
+      puts "SCORE: P1: #{@p1.score} vs P2: #{@p2.score}"
+
+      # set current player
+      if @current_player == @p1
+        @current_player = @p2
+      else 
+        @current_player = @p1
+      end 
+    end
+
+    # decide winner
+    if @p1.score >= 1
+      puts "#{@p1.name} wins with a score of #{@p1.score}"
+    else 
+      puts "#{@p2.name} wins with a score of #{@p2.score}" 
+    end
+    puts "---- GAME OVER ----"
+    puts "GOOD BYE!"
   end
 end
-
